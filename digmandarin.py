3eb1d21bbf4d606215ceb1a8851090f4a314f750
Pierre Brunelle <pierrebrunelleai@gmail.com>
2019-10-05 20:01:13 -0400

    Downloading sounds

--- digmandarin.py
+++ digmandarin.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 '''
 Here are the patterns I have observed:
@@ -20,12 +20,13 @@ Notes:
 - v = u with double dot
 '''
 
+import os
+import os.path
+import random
+import urllib.error
 import urllib.request
 
-urllib.request.urlretrieve(url, file_name)
-
-
-inital2row = {
+INITIALS = {
     '': 'row1',
     'b': 'BPMF', 'p': 'BPMF', 'm': 'BPMF', 'f': 'BPMF',
     'd': 'DTNL', 't': 'DTNL', 'n': 'DTNL', 'l': 'DTNL',
@@ -35,7 +36,7 @@ inital2row = {
     'j': 'JQX', 'q': 'JQX', 'x': 'JQX',
 }
 
-finals = [
+FINALS = [
     'a', 'ai', 'an', 'ang', 'ao',
     'e', 'ei', 'en', 'eng', 'er',
     'i', 'ia', 'iao', 'ie', 'iu', 'ian', 'in', 'iang', 'ing', 'iong',
@@ -44,44 +45,49 @@ finals = [
     'v', 've', 'van', 'vn',
 ]
 
-def get_pinyin(initial, final):
-    if initial != '':
-        return '%s%s' % (initial, final)
-    if final in ('a', 'e', 'o', 'er', 'ai', 'ao', 'ou', 'an', 'en', 'ang', 'eng',):
-        return final
-    if final in ('i', 'in', 'ing',):
-        return 'y%s' % final
-    if final in ('ia', 'iao', 'ie', 'ian', 'iang', 'iong',):
-        return 'y%s' % final{1:]
-    if final == 'iu':
-        return 'you'
-    if final == 'un':
-        return 'wen'
-    if final.startswith('u'):
-        return 'w%s' % final[1:]
-    if final.startswith('v'):
-        return 'y%s' % final[1:]
-    raise Exception('Unexpected (initial, final) = (%s, %s)' % (initial, final))
-
-def get_pinyin_tone(initial, final, tone):
-    initial_final = '%s%s' % (initial, final)
-    if initial == '':
-        initial_final = get_initial_final(final)
-    return '%s%d' % (initial_final, tone)
-
-def get_url(initial, final, tone):
-    return 'https://www.digmandarin.com/tools/sounds/%s/%s/Audio.mp3' %\
-           (inital2row[initial], get_pinyin(initial, final, tone))
+AUDIO_FORMAT = '.mp3'
 
 class PinyinLink:
-    def __init__(self, initial, final, tone, row, url):
+    def __init__(self, initial, final, tone):
         self.initial = initial
         self.final = final
         self.tone = tone
-        self.row = row
-        self.url = url
+        self.pinyin_tone = self.get_pinyin_tone(initial, final, tone)
+        self.url = self.get_url(initial, self.pinyin_tone)
+        self.filename = '%s%s' % (self.pinyin_tone, AUDIO_FORMAT)
 
-def is_valid_initial_final(initial, final):
+    def get_pinyin(self, initial, final):
+        if initial != '':
+            return '%s%s' % (initial, final)
+        if final in ('a', 'e', 'o', 'er', 'ai', 'ao', 'ou', 'an', 'en', 'ang',
+                     'eng', 'ei', 'ong',):
+            return final
+        if final in ('i', 'in', 'ing',):
+            return 'y%s' % final
+        if final in ('ia', 'iao', 'ie', 'ian', 'iang', 'iong',):
+            return 'y%s' % final[1:]
+        if final == 'iu':
+            return 'you'
+        if final == 'un':
+            return 'wen'
+        if final.startswith('u'):
+            return 'w%s' % final[1:]
+        if final.startswith('v'):
+            return 'y%s' % final[1:]
+        raise Exception('Unexpected (initial, final) = (%s, %s)' %\
+                        (initial, final))
+
+    def get_pinyin_tone(self, initial, final, tone):
+        initial_final = '%s%s' % (initial, final)
+        if initial == '':
+            initial_final = self.get_pinyin(initial, final)
+        return '%s%d' % (initial_final, tone)
+
+    def get_url(self, initial, pinyin_tone):
+        return 'https://www.digmandarin.com/tools/sounds/%s/%s/Audio%s' %\
+               (INITIALS[initial], pinyin_tone, AUDIO_FORMAT)
+
+def UNUSED_is_valid_initial_final(initial, final):
     '''
     Return whether the combination (initial, final) is valid according to
     from https://www.digmandarin.com/chinese-pinyin-chart.
@@ -95,7 +101,7 @@ def is_valid_initial_final(initial, final):
                not final.startswith('v') and \
                final != 'ueng' and \
                final != 'er' and \
-               final != 'o':
+               final != 'o'
     return True
 
 def get_links():
@@ -103,6 +109,42 @@ def get_links():
     Return [PinyinLink, ...] for valid combinations (final, initial)
     from https://www.digmandarin.com/chinese-pinyin-chart
     '''
+    ret = [ ]
+    for initial in INITIALS.keys():
+        for final in FINALS:
+            for tone in (1, 2, 3, 4):
+                ret.append(PinyinLink(initial, final, tone))
+    return ret
+
+def get_existing_pinyin_tones(datadir):
+    return set([os.path.basename(fname).replace(AUDIO_FORMAT, '') \
+                for fname in os.listdir(datadir) \
+                if fname.endswith(AUDIO_FORMAT)])
+
+def download_files(datadir):
+    os.makedirs(datadir, exist_ok=True)
+    skip = get_existing_pinyin_tones(datadir)
+    skip_other_tones = set()
+    all_links = get_links()
+    random.shuffle(all_links)
+    links = [l for l in all_links if l.pinyin_tone not in skip]
+    print('Downloading %d sound files (%d total files, %d already downloaded '\
+          'in %s' % (len(links), len(all_links), len(skip), datadir))
+    for i, l in enumerate(links):
+        outfname = '%s/%s%s' % (datadir, l.pinyin_tone, AUDIO_FORMAT)
+        if (l.initial, l.final) in skip_other_tones:
+            print('%d/%d Skipping %s: failure on (initial, final)' %\
+                  (i+1, len(links), outfname))
+            continue
+        print('%d/%d Downloading %s into %s' % (i+1, len(links), l.url, outfname))
+        try:
+            urllib.request.urlretrieve(l.url, outfname)
+        except urllib.error.HTTPError as e:
+            if e.code == 404:
+                skip_other_tones.add((l.initial, l.final))
+                print('not found, skiping other tones of same (initial, final)')
+            else:
+                raise
 
 if __name__ == '__main__':
     datadir = './sounds'
